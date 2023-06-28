7913a85a93d5f891f3576b0ec550a5fd44f017ac
mukoyadarius <mukoya.darius@student.moringaschool.com>
2023-06-28 10:19:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+var dodger = document.getElementById('dodger');
+      
+dodger.style.backgroundColor = "#ff69b4";
+      
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+        
+  if ( left > 0 ) {
+    dodger.style.left = `${left - 4}px`;
+  }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if ( left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
+     
